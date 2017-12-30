dcba99001437b98a0bc9512a139afa9a2be23733
Shichao Ji <jshichao@vt.edu>
2017-12-30 15:33:59 -0500

    0.1

---
+++ picutil/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+from .base import 
+
+__version__ = '0.1'
+__license__ = 'MIT'
+
+__all__ = ['unpack_dict', 'unpack_one_layer_list','series2df', 'LoadFile']
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "picutil",
+    version = "0.1",
+    description = "Hosting pictures into html, website, python Flask and GUI, image manupilation",
+    author = "Shichao(Richard) Ji",
+    author_email = "jshichao@vt.edu",
+    url = "https://github.com/shichaoji2016/picutil",
+    download_url = "https://github.com/shichaoji2016/picutil/archive/0.1.tar.gz",
+    keywords = ['hosting','picture','image','python','flask'],
+    classifiers = [],
+    packages = find_packages(),
+)
+
