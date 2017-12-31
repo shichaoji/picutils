ac7af6fb42cd68ac38e80de11deb3bf98f1f951b
Shichao Ji <jshichao@vt.edu>
2017-12-30 20:34:51 -0500

    add executable

--- picutils/__init__.py
+++ picutils/__init__.py
@@ -7,3 +7,5 @@ __version__ = '0.1'
 __license__ = 'MIT'
 
 __all__ = ['processPics']
+
+
--- setup.py
+++ setup.py
@@ -2,14 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name = "picutils",
-    version = "0.1",
+    version = "0.1.3",
     description = "Hosting pictures into html, website, python Flask and GUI, image manupilation",
     author = "Shichao(Richard) Ji",
     author_email = "jshichao@vt.edu",
     url = "https://github.com/shichaoji2016/picutils",
     download_url = "https://github.com/shichaoji2016/picutils/archive/0.1.tar.gz",
     keywords = ['hosting','picture','image','python','flask'],
-    classifiers = ['Programming Language :: Python :: 2', 'Programming Language :: Python :: 3'],
+    license = 'MIT', 
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        ],
     packages = find_packages(),
+    install_requires=[
+          'Pillow',
+      ]
 )
 
