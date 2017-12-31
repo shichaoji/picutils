bf47abf000a1803b5b87492ac05aea4f80d2dc91
Shichao Ji <jshichao@vt.edu>
2017-12-30 19:08:31 -0500

    fix __init__

--- picutil/__init__.py
+++ picutil/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 
-from .base import 
+from .base import processPics
 
 __version__ = '0.1'
 __license__ = 'MIT'
 
-__all__ = ['unpack_dict', 'unpack_one_layer_list','series2df', 'LoadFile']
+__all__ = ['processPics']
